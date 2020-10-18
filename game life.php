8fe4b6f2e31303b96d7818bfb754419a890f1c89
dmitrylysuk <dimko82@list.ru>
2020-10-18 22:06:11 +0300

    add function getNeighbors for shorten the code

--- game life.php
+++ game life.php
@@ -1,5 +1,5 @@
 <?php 
-// Life v1. /10 steps/
+// Life v2. /10 steps/
 $step = 1;
 $space = [
 ['O','X','O','O','O'],
@@ -8,6 +8,18 @@ $space = [
 ['O','O','O','O','O'],
 ['O','O','O','O','O']
 ];
+
+function getNeighbors($space, $keyRows, $keyColumns)
+{   
+    $workMap = [0, -1, 0, 1, -1, -1, -1, 0, -1, 1, 1, -1, 1, 0, 1, 1];
+    $neighbors = 0;
+        for ($i = 0; $i < 16; $i += 2) {
+            if ($space[$keyRows + $workMap[$i]][$keyColumns + $workMap[$i + 1]] == 'X') {    
+                $neighbors++;
+            }
+        }
+    return $neighbors;
+}
 foreach ($space as $item) {
     foreach ($item as $elem) {
         echo $elem. ' ';
@@ -15,39 +27,15 @@ foreach ($space as $item) {
     echo '<br>';
 }
 echo '<br>';
-for($f = 1 ; $f <= 10; $f++){
+for($f = 1; $f <= 10; $f++){
+
     $col = count($space[0]) - 1;
     foreach ($space as $keyRows => $rows) {
         foreach ($rows as $keyColumns => $cell) {
-            $flag = 0;
-            if ($space[$keyRows][$keyColumns - 1] == 'X') {
-                $flag++;
-            }
-            if ($space[$keyRows][$keyColumns + 1] == 'X') {
-                $flag++;
-            }
-            if ($space[$keyRows - 1][$keyColumns - 1] == 'X') {
-                $flag++;
-            }
-            if ($space[$keyRows - 1][$keyColumns] == 'X') {
-                $flag++;
-            }
-            if ($space[$keyRows - 1][$keyColumns + 1] == 'X') {
-                $flag++;
-            }
-            if ($space[$keyRows + 1][$keyColumns - 1] == 'X') {
-                $flag++;
-            }
-            if ($space[$keyRows + 1][$keyColumns] == 'X') {
-                $flag++;
-            }
-            if ($space[$keyRows + 1][$keyColumns + 1] == 'X') {
-                $flag++;
-            }
-
-            if ($cell == 'O' and $flag == 3) {
+            $neighbors = getNeighbors($space, $keyRows, $keyColumns);
+            if ($cell == 'O' and $neighbors == 3) {
                 $cells[] = ['row' => $keyRows, 'col' => $keyColumns, 'fill' => 'X'];
-            } elseif ($cell == 'X' and ($flag < 2 or $flag > 3)) {
+            } elseif ($cell == 'X' and ($neighbors < 2 or $neighbors > 3)) {
                 $cells[] = ['row' => $keyRows, 'col' => $keyColumns, 'fill' => 'O'];
             } else {
                 $cells[] = ['row' => $keyRows, 'col' => $keyColumns, 'fill' => $cell];
